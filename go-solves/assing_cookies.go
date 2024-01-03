fe1ea50f7d3c63b7613a5fd2b622c48d8978d684
soundOfff <tomas@polluxcoop.com>
2024-01-03 10:17:16 -0500

    solve leetcode 455

---
+++ go-solves/assing_cookies.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"slices"
+)
+
+// Leetcode daily problem, 455 Assign Cookies
+func FindContentChildren(g []int, s []int) int {
+	content := 0
+	slices.Sort(g)
+	slices.Sort(s)
+
+	for i := 0; i < len(s); i++ {
+		ni := binarySearch(s[i], g)
+		if ni == -1 {
+			continue
+		} else {
+			content++
+			g = append(g[:ni], g[ni+1:]...)
+		}
+	}
+	return content
+}
+
+func binarySearch(s int, g []int) int {
+	low := 0
+	high := len(g) - 1
+	for low <= high {
+		median := (low + high) / 2
+		if g[median] <= s {
+			return median
+		} else if g[median] < s {
+			low = median + 1
+		} else {
+			high = median - 1
+		}
+	}
+	if low == len(g) || g[low] > s {
+		return -1
+	}
+	return -1
+}
--- go-solves/main.go
+++ go-solves/main.go
@@ -6,4 +6,6 @@ import "fmt"
 func main() {
 	beams := NumberOfBeams([]string{"011001", "000000", "010100", "001000"})
 	fmt.Println(beams)
+	content := FindContentChildren([]int{10, 9, 8, 7}, []int{10, 9, 8, 7})
+	fmt.Println(content)
 }
