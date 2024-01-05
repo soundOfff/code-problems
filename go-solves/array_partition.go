2f1824881f4cb74d8c8ea1186458d1eaec78d328
soundOfff <tomas@polluxcoop.com>
2024-01-05 15:51:32 -0500

    improve performance in array partition

--- go-solves/array_partition.go
+++ go-solves/array_partition.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"slices"
 )
@@ -18,4 +19,40 @@ func LengthOfLIS(nums []int) int {
 		}
 	}
 	return slices.Max(dp) + 1
+}
+
+// better approach with binary search
+
+func LengthOfLIS2(nums []int) int {
+	sub := make([]int, 0, len(nums)>>1)
+
+	for _, num := range nums {
+		fmt.Println("---------", num, "---------")
+		index := binarySearch(sub, num)
+		if index == len(sub) {
+			sub = append(sub, num)
+		} else {
+			sub[index] = num
+		}
+		fmt.Println(sub, index)
+	}
+
+	return len(sub)
+}
+
+// Esta binaria calcula basandose en si no hay un numero mayor que el parametro num
+// Entonces lo que hace es devolver un indice igual a la longitud y despues en el codigo
+// De la funcion se hace un append para agregar el numero y aumentar la long.
+// En el caso que el numero sea menor que el ultimo numero del slice, entonces se reemplaza.
+func binarySearch(sub []int, num int) int {
+	l, r := -1, len(sub)
+	for r-l > 1 {
+		m := (l + r) / 2
+		if num > sub[m] {
+			l = m
+		} else {
+			r = m
+		}
+	}
+	return r
 }
