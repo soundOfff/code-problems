c813e35cf9bd125dbaac27f60b1164a87c98b170
soundOfff <tomas@polluxcoop.com>
2024-07-07 19:45:58 -0400

    wip round 918

--- cpp/div4/Round 918/e.cpp
+++ cpp/div4/Round 918/e.cpp
@@ -1,9 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 typedef long long ll;
-typedef pair<int,int> ii;
+typedef pair<int, int> ii;
 typedef vector<int> vi;
 typedef vector<ll> vl;
 typedef vector<ii> vp;
@@ -12,7 +11,7 @@ typedef vector<char> vc;
 typedef long double ld;
 
 typedef set<int>::iterator sit;
-typedef map<int,int>::iterator mit;
+typedef map<int, int>::iterator mit;
 typedef vector<int>::iterator vit;
 typedef vector<long long int>::iterator vllit;
 
@@ -20,18 +19,18 @@ const int INF = 1e9 + 7;
 const int MOD = 1e9 + 7;
 const int MAXN = 1e6 + 3;
 
-#define _  %  MOD
+#define _ % MOD
 #define __ %= MOD
 
-#define      each(it,s)        for(vit it = s.begin(); it != s.end(); ++it)
-#define      sortAsc(v)          sort(v.begin(), v.end())
-#define      sortDesc(v)          sort(v.begin(), v.end(), greater<auto>())
-#define      fill(a, v)           memset(a, v, sizeof (a))
+#define each(it, s) for (vit it = s.begin(); it != s.end(); ++it)
+#define sortAsc(v) sort(v.begin(), v.end())
+#define sortDesc(v) sort(v.begin(), v.end(), greater<auto>())
+#define fill(a, v) memset(a, v, sizeof(a))
 
-#define      fori(i, n)         for(int i = 0; i < (n); ++i)
-#define      fora(i, a, n)     for(int i = a; i <= (n); ++i)
-#define      ford(i, a, n)     for(int i = a; i >= (n); --i)
-#define 	 pq(x)			   priority_queue<x,std::vector<x>,compare>
+#define fori(i, n) for (int i = 0; i < (n); ++i)
+#define fora(i, a, n) for (int i = a; i <= (n); ++i)
+#define ford(i, a, n) for (int i = a; i >= (n); --i)
+#define pq(x) priority_queue<x, std::vector<x>, compare>
 
 #define min3(a, b, c) min(a, min(b, c))
 #define max3(a, b, c) max(a, max(b, c))
@@ -49,19 +48,70 @@ const int MAXN = 1e6 + 3;
 #define fbo find_by_order
 #define ook order_of_key
 
-ll solve(ll l, ll r) {
-    
+void fill_odd(vi arr, int n, vi prefix)
+{
+    prefix[0] = arr[0];
+    fori(i, n)
+    {
+        if (i % 2 == 0)
+        {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+        else
+        {
+            prefix[i] = prefix[i - 1];
+        }
+    }
 }
 
-//g++ -std=c++11 -O2 -Wall test.cpp -o test
-int main() {
+void fill_even(vi arr, int n, vi prefix)
+{
+    prefix[0] = 0;
+    fori(i, n)
+    {
+        if (i % 2 != 0)
+        {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+        else
+        {
+            prefix[i] = prefix[i - 1];
+        }
+    }
+}
+
+// g++ -std=c++11 -O2 -Wall test.cpp -o test
+int main()
+{
     int t;
     cin >> t;
     while (t--)
     {
         int n;
+        vi a;
+        vi prefix_odd(n);
+        vi prefix_even(n);
         cin >> n;
+        fori(i, n)
+        {
+            cin >> a[i];
+        }
+        fill_odd(a, n, prefix_odd);
+        fill_even(a, n, prefix_even);
 
+        bool flag = false;
+        fori(k, n)
+        {
+            if (prefix_odd[k] == prefix_even[k])
+            {
+                cout << "YES" << endl;
+                flag = true;
+                break;
+            }
+        }
+        if (!flag)
+        {
+            cout << "NO" << endl;
+        }
     }
-    
 }
